7155472b3450cf5f13126cc408fbebb148c42947
Nicholas Jones <nickjones33@gmail.com>
2021-03-31 17:03:20 -0500

    cleanup

--- FarkleSim/Logic/Scorer.cs
+++ FarkleSim/Logic/Scorer.cs
@@ -13,12 +13,12 @@ namespace FarkleSim.Logic
         public List<ScoringCombination> GetPlays()
         {
             var rtn = new List<ScoringCombination>();
-            if (Combinations.ThreeOfAKind.Ace(_dice)) rtn.Add(new ScoringCombination("Three Aces", 300));
-            if (Combinations.ThreeOfAKind.Two(_dice)) rtn.Add(new ScoringCombination("Three Twos", 200));
-            if (Combinations.ThreeOfAKind.Three(_dice)) rtn.Add(new ScoringCombination("Three Threes", 300));
-            if (Combinations.ThreeOfAKind.Four(_dice)) rtn.Add(new ScoringCombination("Three Fours", 400));
-            if (Combinations.ThreeOfAKind.Five(_dice)) rtn.Add(new ScoringCombination("Three Fives", 500));
-            if (Combinations.ThreeOfAKind.Six(_dice)) rtn.Add(new ScoringCombination("Three Sixes", 600));
+            if (Combinations.ThreeOfAKind.Ace(_dice)) rtn.Add(new ScoringCombination("Three of a kind - Aces", 300));
+            if (Combinations.ThreeOfAKind.Two(_dice)) rtn.Add(new ScoringCombination("Three of a kind - Twos", 200));
+            if (Combinations.ThreeOfAKind.Three(_dice)) rtn.Add(new ScoringCombination("Three of a kind - Threes", 300));
+            if (Combinations.ThreeOfAKind.Four(_dice)) rtn.Add(new ScoringCombination("Three of a kind - Fours", 400));
+            if (Combinations.ThreeOfAKind.Five(_dice)) rtn.Add(new ScoringCombination("Three of a kind - Fives", 500));
+            if (Combinations.ThreeOfAKind.Six(_dice)) rtn.Add(new ScoringCombination("Three of a kind - Sixes", 600));
             if (Combinations.FourOfAKind(_dice)) rtn.Add(new ScoringCombination("Four of a kind", 1000));
             if (Combinations.FiveOfAKind(_dice)) rtn.Add(new ScoringCombination("Five of a kind", 2000));
             if (Combinations.SixOfAKind(_dice)) rtn.Add(new ScoringCombination("Six of a kind", 3000));
